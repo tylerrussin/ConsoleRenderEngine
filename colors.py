4aced053bd12c794fbb6d304d7a19406f38406bb
tyler9937 <tyler9937@users.noreply.github.com>
2020-06-09 00:27:11 -0400

    added a memery cahce

--- colors.py
+++ colors.py
@@ -1,11 +1,28 @@
+'''
+Script testing out color rendering in the command prompt
+'''
 import curses
 import time
+
+from curses import has_colors, can_change_color
+
+# Initiate classes
 view = curses.initscr()
 curses.start_color()
-curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+
+# Does command prompt support color?
+print(has_colors())
+
+# Can we change colors?
+print(can_change_color())
+
+# color integer is 0 - 767
+# zero reserved for black
+curses.init_color(42, 66, 182, 245)
+
+# All color pallets
+curses.init_pair(1, 42, curses.COLOR_BLACK)
 view.addstr(10,10,"Pretty text", curses.color_pair(1))
 view.refresh()
-time.sleep(10)
+time.sleep(4)
 
-y = "\33[32m#\033[0m"
-print(y)
