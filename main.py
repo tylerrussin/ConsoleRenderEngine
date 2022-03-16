296b0c508c2519b11ed8c7bd8738269dab822b34
Tyler Russin <tylerrussin2@gmail.com>
2022-03-15 22:17:27 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import curses
 
 from Command_Line_Font import command_line_font
 
+
 command_line_font(16)        # Console ASCII font size
 nScreenWidth = 120          # Console Screen Size X (columns)
 nScreenHeight = 40          # Console Screen Size Y (rows)
@@ -13,6 +14,49 @@ nScreenHeight = 40          # Console Screen Size Y (rows)
 screen = [[' ' for x in range(nScreenWidth)] for y in range(nScreenHeight)]
 os.system(f"mode con: cols={nScreenWidth} lines={nScreenHeight}")
 view = curses.initscr()
+
+class Vec_3D():
+
+    x = 0
+    y = 0
+    z = 0
+
+class Triangle():
+
+    points = [Vec_3D(), Vec_3D(), Vec_3D()]
+
+class Mesh():
+
+    tris = [Triangle()]
+
+class Cube_Mesh():
+
+    tris = [
+
+        # South
+        [0, 0, 0,       0, 1, 0,        1, 1, 0],
+        [0, 0, 0,       1, 1, 0,        1, 0, 0],
+
+        # East
+        [1, 0, 0,       1, 1, 0,        1, 1, 1],
+        [1, 0, 0,       1, 1, 1,        1, 0, 1],
+
+        # North
+        [1, 0, 1,       1, 1, 1,        0, 1, 1],
+        [1, 0, 1,       0, 1, 1,        0, 0, 1],
+
+        # West
+        [0, 0, 1,       0, 1, 1,        0, 1, 0],
+        [0, 0, 1,       0, 1, 0,        0, 0, 0],
+
+        # Top
+        [0, 1, 0,       0, 1, 1,        1, 1, 1],
+        [0, 1, 0,       1, 1, 1,        1, 1, 0],
+
+        # Bottom
+        [1, 0, 1,       0, 0, 1,        0, 0, 0],
+        [1, 0, 1,       0, 0, 0,        1, 0, 0]
+    ]
 
 
 while 1:
@@ -24,6 +68,9 @@ while 1:
         command_line_font(16)                           # Reset default font size
         sys.exit()                                      # Exit Program
 
+    # Draw Triangles
+    for tri in Cube_Mesh.tris:
+        pass
 
     screen[20][40] = 'T'
 
